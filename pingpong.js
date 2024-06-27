32c24fa9044ca9d8617bd1f93b8ac8eecb95ca21
kadu-passos <168776441+kadu-passos@users.noreply.github.com>
2024-06-27 17:41:38 -0300

    cria bolinha 27/06/2024

--- pingpong.js
+++ pingpong.js
@@ -1,3 +1,7 @@
+// aqui eu vou criar a bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let tambolinha = 25
 function setup(){
     //aqui vou criar minha "mesa"
     createCanvas(600,400);
@@ -6,4 +10,11 @@ function setup(){
 function draw (){
     //aqui vou por a cor da "mesa"
     background(176,224,230);
+
+    criabolinha (xBolinha, yBolinha, tambolinha);
+}
+
+//função bolinha
+function criabolinha (xBolinha, yBolinha, tambolinha){
+    circle (xBolinha, yBolinha, tambolinha);
 }

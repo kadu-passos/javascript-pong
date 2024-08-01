c3dc6a5ddecc12872cb141db57e84d3529d05809
kadu-passos <168776441+kadu-passos@users.noreply.github.com>
2024-08-01 16:31:31 -0300

    raquete

--- pingpong.js
+++ pingpong.js
@@ -2,10 +2,17 @@
 let xBolinha = 300;
 let yBolinha = 200;
 let tamBolinha = 25;
+let raioBolinha = 25;
 
 //config velocidade bolinha
 let xvelocidadeBolinha = 6;
 let yvelocidadeBolinha = 6;
+
+//configura raquete
+let xRaquete = 5;
+let yRaquete = 150;
+let larguraRaquete = 10;
+let alturaRaquete = 90;
 
 function setup(){
     //aqui vou criar minha "mesa"
@@ -16,13 +23,16 @@ function draw(){
     //função responsável pelo "desenho e animação da mesa"
     //aqui vou por a cor da "mesa"
     //A cor da mesa é em rgb
-    background(128,128,0); 
+    background(0,0,0); 
     //chamando a função cria bolinha para criar a bolinha
     criaBolinha(xBolinha, yBolinha, tamBolinha);
     //chamando a função mov bolinha
     moveBolinha();
     //chamando a borda
     Borda();
+    criaRaquete(xRaquete, yRaquete, larguraRaquete, alturaRaquete);
+    movimentaRaquete();
+    colideRaquete();
 }
 
 //função bolinha
@@ -45,7 +55,25 @@ function Borda(){
     }
 }
 
-//função cria raquete
-function criaRaquete(){
-    
+//função responsável por cirar o retângulo que representa a raquete
+function criaRaquete(xRaquete, yRaquete, larguraRaquete, alturaRaquete){
+    fill("blue");
+    rect(xRaquete, yRaquete, larguraRaquete, alturaRaquete);
+}
+//função reponsável por movimentar a raquete
+function movimentaRaquete() {
+    if(keyIsDown(UP_ARROW)){
+        yRaquete -= 10;
+    }
+    if(keyIsDown(DOWN_ARROW)){
+        yRaquete += 10;
+    }
+}
+//função responsável por quando a bolinha bater na raquete, retornar em diração contrária.
+function colideRaquete(){
+    if(xBolinha - raioBolinha < xRaquete + larguraRaquete && yBolinha - raioBolinha < yRaquete + alturaRaquete && yBolinha + raioBolinha > yRaquete){
+        
+        xvelocidadeBolinha *= -1;
+        
+    }
 }
